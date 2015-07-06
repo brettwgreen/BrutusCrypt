3f252bffc815c626b1665539d3d6de1db0c6bdf0
brettwgreen <bw_green@yahoo.com>
2015-07-06 15:39:49 -0400

    Tweaks and comments

---
+++ app/src/main/java/com/nervii/brutuscrypt/Cipher.java
@@ -0,0 +1,95 @@
+package com.nervii.brutuscrypt;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Brett on 6/25/2015.
+ */
+public class Cipher
+{
+    public static String RandomizeCipher(String alpha){
+        String randomCipher = "";
+        ArrayList<Character> alphaArray = new ArrayList<>();
+        for (int i=0; i<alpha.length(); i++){
+            alphaArray.add(alpha.charAt(i));
+        }
+        Random rand = new Random();
+        int randomNum;
+        while (alphaArray.size() > 0) {
+            if  (alphaArray.size() == 1) {
+                randomNum = 0;
+            }
+            else {
+                randomNum = rand.nextInt(alphaArray.size() - 1) + 1;
+                if (randomNum > alphaArray.size() - 1) {
+                    randomNum = alphaArray.size() - 1;
+                }
+            }
+
+            randomCipher += alphaArray.get(randomNum);
+            alphaArray.remove(randomNum);
+        }
+        return randomCipher;
+    }
+
+    public static String CaesarShift(String alpha, Character fromCharacter){
+        String shiftCipher = "";
+        ArrayList<Character> alphaArray = new ArrayList<>();
+        for (int i=0; i<alpha.length(); i++){
+            alphaArray.add(alpha.charAt(i));
+        }
+        int start = alphaArray.indexOf(fromCharacter);
+        for (int i=start; i<alphaArray.size(); i++) {
+            shiftCipher += alphaArray.get(i);
+        }
+        for (int i=0; i<start; i++) {
+            shiftCipher += alphaArray.get(i);
+        }
+        return shiftCipher;
+    }
+
+    public static String CaesarShift(String alpha){
+        String shiftCipher = "";
+        ArrayList<Character> alphaArray = new ArrayList<>();
+        for (int i=0; i<alpha.length(); i++){
+            alphaArray.add(alpha.charAt(i));
+        }
+        Random rand = new Random();
+        int randomNum;
+        randomNum = rand.nextInt(alphaArray.size() - 1);
+
+        for (int i=randomNum; i<alphaArray.size(); i++) {
+            shiftCipher += alphaArray.get(i);
+        }
+        for (int i=0; i<randomNum; i++) {
+            shiftCipher += alphaArray.get(i);
+        }
+        return shiftCipher;
+    }
+
+    public static String EncryptMessage(String s, String alpha, String cipher) {
+        if (alpha.length() != cipher.length()) {
+            throw new IllegalArgumentException("alpha and cipher string must have same length");
+        }
+        String eMsg = "";
+        for (int i=0; i < s.length(); i++) {
+            String character = Character.toString(s.charAt(i));
+            String charUpper = character.toUpperCase();
+            boolean lowerCase = false;
+            if (!charUpper.equals(character)) {
+                lowerCase = true;
+            }
+            int index = alpha.indexOf(charUpper);
+            if (index == -1) {
+                eMsg += character;
+            }
+            else {
+                String eChar = Character.toString(cipher.charAt(index));
+                eMsg += lowerCase ? eChar.toLowerCase() : eChar;
+            }
+        }
+        return eMsg;
+    }
+}
--- app/src/main/java/com/nervii/brutuscrypt/Decipher.java
+++ app/src/main/java/com/nervii/brutuscrypt/Decipher.java
@@ -29,22 +29,36 @@ public class Decipher {
 
     public static CrackedPackage CrackShiftCipher(String encryptedMessage) {
         CrackedPackage result = new CrackedPackage();
-        String cipher = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String decryptedMessage = decodeMessage(encryptedMessage, cipher);
-        String[] words = decryptedMessage.split(" ");
         WordList wordList = WordList.getInstance();
+
+        String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // Get a caesar cipher starting from A
+        String cipher = Cipher.CaesarShift(alpha, 'A');
+
+        // Use that cipher and decrypt the message
+        String decryptedMessage = decodeMessage(encryptedMessage, cipher);
+
+        // Get all the words in the message
+        String[] words = decryptedMessage.split(" ");
         boolean cracked = true;
+
+        // See if all the words are in our english word list
         for (int i=0; i<words.length; i++) {
             if (!wordList.IsWordInList(words[i])) {
+                // if even one word is not found,
+                // the cipher is wrong
+                // This could be refined... maybe to set a threshold on 70% or words or something
                 cracked = false;
             }
         }
+        // if we cracked it, fill in the accepted cipher and decrypted message
         if (cracked) {
             result.Cipher = cipher;
             result.DecryptedMessage = decryptedMessage;
         }
+        // Otherwise just set the cipher to 'Unable to Crack'
         else {
-            result.Cipher = "UNABLE TO CRACK";
+            result.Cipher = "--UNABLE TO CRACK--";
             result.DecryptedMessage = "";
         }
         return result;
